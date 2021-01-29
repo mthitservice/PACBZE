0252f2f8ffa13619a71a7560712ed6b0e5507cf9
Michael Lindner <michael.lindner@mthweb.de>
2021-01-29 12:02:33 +0100

    GameLoop Begin

--- classes/PacBzeApp.cs
+++ classes/PacBzeApp.cs
@@ -4,10 +4,44 @@ namespace PACBZE.classes
 {
    public class PacBzeApp 
     {              
-
+        private Gamer _currenGamer;
         public void start()
         {
-            
+
+            // Eingabe
+            // Spielernamen erfragen
+            this.get_Gamer();
+
+            // Spielfeld darstellen
+
+            // Spiel starten
+                // Tastendruck SPieler auswerten
+                // Spielfiguren setzen
+                // Spiellregeln prüfen
+            // Wenn Spielzustand Gamer Over -> Ende
+            // Wenn Spielzustand Succesfuly -> Ende mit Gratulation
+            // Wenn Spielzustand nächster Zug - Springe zu "Spiel STarte"
+
+
+
+            // Verarbeitung 
+            // Ende mit Gratulation : Ausgabe Herzlichen Glückwunsch
+            // Ende: Ausgabe Heighscore
+
+            // AUsgabe
+        }
+        // Konstruktor
+        public PacBzeApp()
+        {
+            this._currenGamer=new Gamer();
+
+        }
+
+        // Spielmethoden
+        private void get_Gamer()
+        {
+
+
         }
 
     }
